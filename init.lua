42449c695372d3fca0b5960aacab0f26cc810ad1
Joe Searle <joe@jsearle.net>
2023-06-13 17:37:44 +0000

    test

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 vim.g.lua_snippets_path = "~/.config/nvim/lua/custom/lua_snippets"
 
+require("luasnip").filetype_extend("javascript", { "javascriptreact" })
+require("luasnip").filetype_extend("javascript", { "html" })
+
